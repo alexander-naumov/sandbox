c41e0fbf319a73fabd4cb1a73c0ac3323b85990f
anaumov <posix.ru@gmail.com>
2014-06-01 14:10:16 +0200

    Update scanner.lisp

--- lisp/scanner.lisp
+++ lisp/scanner.lisp
@@ -3,15 +3,16 @@
 (load "~/quicklisp/setup.lisp")  
 (ql:quickload "usocket")
 
+
 (defun scan (ports)
     "show open ports"
-        (loop for p in ports
-            do (ignore-errors
-                (let* ((socket (usocket:socket-connect "openbsd.org" p))
-                    (stream (usocket:socket-stream socket)))
-                        (let ((result (read-line stream)))
-                            (close stream)
-                                (usocket:socket-close socket)
-                                    (format t "~%~d" result))))))
+    (loop for p in ports
+        do (ignore-errors
+        (let* ((socket (usocket:socket-connect "openbsd.org" p))
+            (stream (usocket:socket-stream socket)))
+        (let ((result (read-line stream)))
+            (close stream)
+            (usocket:socket-close socket)
+            (format t "~%~d" result))))))
  
- (scan (list 20 21 22))
+(scan (list 20 21 22))
