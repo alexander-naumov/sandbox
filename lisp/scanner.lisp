c76853f7b20c8ed5913b9fd8de156f4bd6c8d944
Alexander Naumov <posix.ru@gmail.com>
2015-07-13 10:49:19 +0200

    small update

--- lisp/scanner.lisp
+++ lisp/scanner.lisp
@@ -1,18 +1,17 @@
 #!/usr/bin/clisp
 
-(load "~/quicklisp/setup.lisp")  
+(load "~/quicklisp/setup.lisp")
 (ql:quickload "usocket")
 
+(defun scan (host ports)
+	(loop for p in ports
+		do (ignore-errors
+			(format t "~%port ~A: " p)
+			(let* ((socket (usocket:socket-connect host (parse-integer p)))
+				(stream (usocket:socket-stream socket)))
+				(let ((result (read-line stream)))
+					(close stream)
+					(usocket:socket-close socket)
+					(format t "~A~%" result))))))
 
-(defun scan (ports)
-    "show open ports"
-    (loop for p in ports
-        do (ignore-errors
-        (let* ((socket (usocket:socket-connect "openbsd.org" p))
-            (stream (usocket:socket-stream socket)))
-        (let ((result (read-line stream)))
-            (close stream)
-            (usocket:socket-close socket)
-            (format t "~%~d" result))))))
- 
-(scan (list 20 21 22))
+(scan (car *args*) (cdr *args*))
